40126aa54f873e9c4cd693235ecdf580894aca94
Mr. Tee <tullah.nazari@gmail.com>
2019-08-14 08:06:46 -0400

    added a couple more rest calls with mock data

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+
+app.use(morgan('short'))
+
+app.get("/", (req, res) => {
+console.log("Responding to root route")
+res.send("I AM ALIVE!!!!")
+})
+
+app.get("/users", (req, res) => {
+    var user1 = {firstName: "Stephen", lastName: "Curry"}
+    const user2 = {firstName: "Kevin", lastName: "Durant"}
+    res.json([user1, user2])
+
+    }
+    )
+
 
 
 //localhost:3003
 app.listen(3003, () => {
     console.log("Server is up and listening on 3003...")
-})///
+})
