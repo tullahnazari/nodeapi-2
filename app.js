07e5d3d5b58db5042e80eb92634d7b9c25dcd5ee
Mr. Tee <tullah.nazari@gmail.com>
2019-08-15 05:37:07 -0400

    Adding get request and connecting to db
    -Adding get request
    -connecting to db
    -chaning attribute text from snake to camel case

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const mysql = require('mysql')
+
 
 app.use(morgan('short'))
+
+app.get('/user/:id', (req, res) => {
+    console.log("fetching user with id: " + req.params.id)
+
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'wahdat08',
+        database: 'nametesting'
+    })
+
+    const userId = req.params.id
+    const queryString = "SELECT * FROM users WHERE id = ?"
+    connection.query(queryString, [userId], (err, rows, fields) => {
+    if (err) {
+        console.log("Failed to query for users: " + err)
+        res.sendStatus(500)
+        return
+    }
+        console.log("I think we fetched users successfully")
+
+        //to make the attibute names camelCase instead of snakecase
+        const users = rows.map((row) => {
+            return {firstName: row.first_name, lastName: row.last_name}
+        })
+        res.json(users)
+    })
+    //res.end()
+})
 
 app.get("/", (req, res) => {
 console.log("Responding to root route")
