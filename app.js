de68433a07a313b6da5a46ef80692bfd775a060e
Mr. Tee <tullah.nazari@gmail.com>
2019-08-15 08:12:25 -0400

    Added post method and linked it with simple form
    - Added post
    - linked it with some UI
    - created a fun for 'getconnection' to connect to our db

--- app.js
+++ app.js
@@ -3,18 +3,55 @@ const app = express()
 const morgan = require('morgan')
 const mysql = require('mysql')
 
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(express.static('./public'))
 
 app.use(morgan('short'))
 
-app.get('/user/:id', (req, res) => {
-    console.log("fetching user with id: " + req.params.id)
+app.post('/user_create', (req, res) => {
+    console.log("Trying to create a new user...")
+    console.log("How do we get form data?")
 
-    const connection = mysql.createConnection({
+    console.log("First name: ", req.body.create_first_name)
+    const firstName = req.body.create_first_name
+    const lastName = req.body.create_last_name
+
+    const queryString = "INSERT INTO users (first_name, last_name) VALUES (?, ?)"
+    getConnection().query(queryString, [firstName, lastName], (err, results, fields) => 
+    {
+        if (err) {
+            console.log("fail to insert new user: " + err)
+            res.sendStatus(500)
+            return
+        }
+
+        console.log("Inserted new user with id: ", results.insertedId);
+    })
+
+
+
+
+
+
+    res.end()
+})
+
+function getConnection() {
+    return mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: 'wahdat08',
         database: 'nametesting'
     })
+}
+
+app.get('/user/:id', (req, res) => {
+    console.log("fetching user with id: " + req.params.id)
+
+    const connection = getConnection()
 
     const userId = req.params.id
     const queryString = "SELECT * FROM users WHERE id = ?"
